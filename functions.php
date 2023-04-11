1f74014bcd2ce5783c78e5a5aebe11e707741c22
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-04-11 10:51:10 +0200

    Essai BDD prod

--- functions.php
+++ functions.php
@@ -7,7 +7,8 @@ $pass = 'root';
 
 try
 {
-  $db = new PDO('mysql:host=localhost;dbname=form', $user, $pass);
+  // $db = new PDO('mysql:host=localhost;dbname=form', $user, $pass);
+  $db = new PDO("us-cdbr-east-06.cleardb.net", "bf61bcf48e529d", "d14b5f42", "heroku_ed494a7436c047c");
 } catch (PDOException $e)
 
 {
